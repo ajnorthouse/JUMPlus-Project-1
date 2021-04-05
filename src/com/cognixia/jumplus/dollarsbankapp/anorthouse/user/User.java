41ef1e44b403f280251db14decaf8e673d8604c5
Alexandre Northouse <Alex.J.Northouse@gmail.com>
2021-04-05 12:42:15 -0400

    Redo user structure

--- src/com/cognixia/jumplus/dollarsbankapp/anorthouse/user/User.java
+++ src/com/cognixia/jumplus/dollarsbankapp/anorthouse/user/User.java
@@ -1,8 +1,13 @@
 package com.cognixia.jumplus.dollarsbankapp.anorthouse.user;
 
 import java.util.ArrayList;
-
+/**
+ * 
+ * @author alexn
+ *
+ */
 public class User {
+//	Class Variables
 	private static Long currentID = 0L;
 	private Long id;
 	private String userId;
@@ -12,13 +17,12 @@ public class User {
 	private double balance;
 	private ArrayList<String> log;
 	
-	//constructors:
-	// empty constructor
+	
+//	Constructors
 	public User() {
 		super();
 		id = currentID++;
 	}
-	// default constructor
 	public User(String userId, String password, String name, String contactNumber, double balance) {
 		super();
 		this.userId = userId;
@@ -28,50 +32,50 @@ public class User {
 		this.balance = balance;
 		this.log = new ArrayList<String>();
 	}
-
-	/* userId methods */
+	
+	
+//	Getters
+	public static Long getCurrentID() {
+		return currentID;
+	}
+	public Long getId() {
+		return id;
+	}
 	public String getUserId() {
 		return userId;
 	}
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
-	/* password methods */
 	public String getPassword() {
 		return password;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getContactNumber() {
+		return contactNumber;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public ArrayList<String> getLog() {
+		return log;
+	}
+	
+	
+//	Setters
+	public void setUserId(String userId) {
+		this.userId = userId;
 	}
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-	/* name methods */
-	public String getName() {
-		return name;
-	}
 	public void setName(String name) {
 		this.name = name;
-	}
-
-	/* contactNumber methods */
-	public String getContactNumber() {
-		return contactNumber;
 	}
 	public void setContactNumber(String contactNumber) {
 		this.contactNumber = contactNumber;
 	}
-	
-	/* balance methods */
-	public double getBalance() {
-		return balance;
-	}
 	public void setBalance(double balance) {
 		this.balance = balance;
-	}
-
-	/* log methods */
-	public ArrayList<String> getLog() {
-		return log;
 	}
 	public void setLog(ArrayList<String> log) {
 		this.log = log;
