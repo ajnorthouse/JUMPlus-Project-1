53222c1f6f47a4abdbd292b79e4167ef05c988e6
Alexandre Northouse <Alex.J.Northouse@gmail.com>
2021-04-05 12:25:35 -0400

    Setup default user model class

--- src/com/cognixia/jumplus/dollarsbankapp/anorthouse/user/User.java
+++ src/com/cognixia/jumplus/dollarsbankapp/anorthouse/user/User.java
@@ -3,6 +3,7 @@ package com.cognixia.jumplus.dollarsbankapp.anorthouse.user;
 import java.util.ArrayList;
 
 public class User {
+	private static Long currentID = 0L;
 	private Long id;
 	private String userId;
 	private String password;
@@ -11,12 +12,13 @@ public class User {
 	private double balance;
 	private ArrayList<String> log;
 	
-	//empty constructor
+	//constructors:
+	// empty constructor
 	public User() {
 		super();
+		id = currentID++;
 	}
-
-	//default constructor
+	// default constructor
 	public User(String userId, String password, String name, String contactNumber, double balance) {
 		super();
 		this.userId = userId;
@@ -27,49 +29,52 @@ public class User {
 		this.log = new ArrayList<String>();
 	}
 
+	/* userId methods */
 	public String getUserId() {
 		return userId;
 	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
 
+	/* password methods */
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
+	/* name methods */
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/* contactNumber methods */
 	public String getContactNumber() {
 		return contactNumber;
 	}
-
 	public void setContactNumber(String contactNumber) {
 		this.contactNumber = contactNumber;
 	}
-
+	
+	/* balance methods */
 	public double getBalance() {
 		return balance;
 	}
-
 	public void setBalance(double balance) {
 		this.balance = balance;
 	}
 
+	/* log methods */
 	public ArrayList<String> getLog() {
 		return log;
 	}
-
 	public void setLog(ArrayList<String> log) {
 		this.log = log;
 	}
-	
 	
 }
