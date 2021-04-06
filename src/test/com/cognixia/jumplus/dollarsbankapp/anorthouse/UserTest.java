7b1906c7d8a633693442039931df5fbe731bfea1
Alexandre Northouse <Alex.J.Northouse@gmail.com>
2021-04-06 13:32:42 -0400

    Add test class

---
+++ src/test/com/cognixia/jumplus/dollarsbankapp/anorthouse/UserTest.java
@@ -0,0 +1,25 @@
+package com.cognixia.jumplus.dollarsbankapp.anorthouse;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
