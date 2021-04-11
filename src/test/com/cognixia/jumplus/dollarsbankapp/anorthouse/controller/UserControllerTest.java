3b6b151eb1c3c085a768a38c0b66e58b06968a11
Alexandre Northouse <Alex.J.Northouse@gmail.com>
2021-04-11 17:25:24 -0400

    Setup base implementation

---
+++ src/test/com/cognixia/jumplus/dollarsbankapp/anorthouse/controller/UserControllerTest.java
@@ -0,0 +1,183 @@
+package com.cognixia.jumplus.dollarsbankapp.anorthouse.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import com.cognixia.jumplus.dollarsbankapp.anorthouse.application.UserView;
+import com.cognixia.jumplus.dollarsbankapp.anorthouse.model.User;
+
+class UserControllerTest {
+	//class objects for testing:
+	static User testGetUser = new User("Foobar", "password", "John Doe", "8885558888", 100.0);
+	static UserView testView = new UserView();
+	static UserController testController = new UserController(testGetUser, testView);
+
+	/*
+		@DisplayName("Set User Id")
+		@Test
+		void testSetUserId() {
+			String expected = "BarFoo";
+			testSetUser.setUserId(expected);
+			String actual = testSetUser.getUserId();
+			assertEquals(expected, actual);
+		}
+	 */
+	
+	@DisplayName("")
+	@Test
+	final void testGetUserId() {
+		Long expected = 1L;
+		Long actual = testController.getUserId();
+		assertEquals(expected, actual);
+	}
+
+	@DisplayName("")
+	@Test
+	final void testGetUserUserId() {
+		// TODO
+		Object expected = null;
+		Object actual = null;
+		assertEquals(expected, actual);
+	}
+
+	@DisplayName("")
+	@Test
+	final void testSetUserUserId() {
+		// TODO
+		Object expected = null;
+		Object actual = null;
+		assertEquals(expected, actual);
+	}
+
+	@DisplayName("")
+	@Test
+	final void testGetUserPassword() {
+		// TODO
+		Object expected = null;
+		Object actual = null;
+		assertEquals(expected, actual);
+	}
+
+	@DisplayName("")
+	@Test
+	final void testSetUserPassword() {
+		// TODO
+		Object expected = null;
+		Object actual = null;
+		assertEquals(expected, actual);
+	}
+
+	@DisplayName("")
+	@Test
+	final void testGetUserName() {
+		// TODO
+		Object expected = null;
+		Object actual = null;
+		assertEquals(expected, actual);
+	}
+
+	@DisplayName("")
+	@Test
+	final void testSetUserName() {
+		// TODO
+		Object expected = null;
+		Object actual = null;
+		assertEquals(expected, actual);
+	}
+
+	@DisplayName("")
+	@Test
+	final void testGetUserContactNumber() {
+		// TODO
+		Object expected = null;
+		Object actual = null;
+		assertEquals(expected, actual);
+	}
+
+	@DisplayName("")
+	@Test
+	final void testSetUserContactNumber() {
+		// TODO
+		Object expected = null;
+		Object actual = null;
+		assertEquals(expected, actual);
+	}
+
+	@DisplayName("")
+	@Test
+	final void testGetUserBalance() {
+		// TODO
+		Object expected = null;
+		Object actual = null;
+		assertEquals(expected, actual);
+	}
+
+	@DisplayName("")
+	@Test
+	final void testSetUserBalance() {
+		// TODO
+		Object expected = null;
+		Object actual = null;
+		assertEquals(expected, actual);
+	}
+
+	@DisplayName("")
+	@Test
+	final void testGetUserLog() {
+		// TODO
+		Object expected = null;
+		Object actual = null;
+		assertEquals(expected, actual);
+	}
+
+	@DisplayName("")
+	@Test
+	final void testSetUserLog() {
+		// TODO
+		Object expected = null;
+		Object actual = null;
+		assertEquals(expected, actual);
+	}
+
+	@DisplayName("")
+	@Test
+	final void testUpdateView() {
+		// TODO
+		Object expected = null;
+		Object actual = null;
+		assertEquals(expected, actual);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
