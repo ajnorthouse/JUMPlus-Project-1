4c246732e4717b9e7bf01c74fea567632d1026ee
Alexandre Northouse <Alex.J.Northouse@gmail.com>
2021-04-11 18:36:09 -0400

    Redo testing implementation

--- src/test/com/cognixia/jumplus/dollarsbankapp/anorthouse/model/UserTest.java
+++ src/test/com/cognixia/jumplus/dollarsbankapp/anorthouse/model/UserTest.java
@@ -5,62 +5,90 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+@TestMethodOrder(MethodOrderer.DisplayName.class)
 @DisplayName("Test User Model")
 class UserTest {
 	//class objects needed for testing
-	static User testGetUser = new User("Foobar", "password", "John Doe", "8885558888", 100.0);
-	static User testSetUser = new User("Foobar", "password", "John Doe", "8885558888", 100.0);
-	static User testDefaultUser = new User();
-	
+	static User testUser = new User();
+
+	@DisplayName("Constructors")
 	@Nested
-	@DisplayName("Setters")
+	class constructors {
+
+		@DisplayName("Default Values Test")
+		@Test
+		void testDefaultConstructor() {
+			Boolean[] expected = {true, true, true, true, true, true, true};
+			Boolean[] actual = {
+					(testUser.getId() == 2), 
+					(testUser.getUserId() == "N/A"), 
+					(testUser.getPassword() == ""), 
+					(testUser.getName() == "N/A"), 
+					(testUser.getContactNumber() == "N/A"), 
+					(testUser.getBalance() == 0.0), 
+					(testUser.getLog() == null)
+			};
+			assertArrayEquals(expected, actual);
+		}
+
+		@DisplayName("Auto Incrementing ID Test")
+		@Test
+		void testCurrentIdIncrement() {
+			assertEquals(3L, User.getCurrentID());
+		}
+	}
+
+	@DisplayName("Setter Methods")
+	@Nested
 	class setters {
 		
 		@DisplayName("Set User Id")
 		@Test
-		void testSetUserId() {
-			String expected = "BarFoo";
-			testSetUser.setUserId(expected);
-			String actual = testSetUser.getUserId();
+		void testUserId() {
+			String expected = "Foobar";
+			testUser.setUserId(expected);
+			String actual = testUser.getUserId();
 			assertEquals(expected, actual);
 		}
 		
 		@DisplayName("Set Password")
 		@Test
 		void testSetPassword() {
-			String expected = "drowssap";
-			testSetUser.setPassword(expected);
-			String actual = testSetUser.getPassword();
+			String expected = "password";
+			testUser.setPassword(expected);
+			String actual = testUser.getPassword();
 			assertEquals(expected, actual);
 		}
 		
 		@DisplayName("Set Name")
 		@Test
 		void testSetName() {
-			String expected = "Jane Dane";
-			testSetUser.setName(expected);
-			String actual = testSetUser.getName();
+			String expected = "John Doe";
+			testUser.setName(expected);
+			String actual = testUser.getName();
 			assertEquals(expected, actual);
 		}
 		
 		@DisplayName("Set Contact Number")
 		@Test
 		void testSetContactNumber() {
-			String expected = "5558885555";
-			testSetUser.setContactNumber(expected);
-			String actual = testSetUser.getContactNumber();
+			String expected = "8885558888";
+			testUser.setContactNumber(expected);
+			String actual = testUser.getContactNumber();
 			assertEquals(expected, actual);
 		}
 		
 		@DisplayName("Set Balance")
 		@Test
 		void testSetBalance() {
-			double expected = 1000.00;
-			testSetUser.setBalance(expected);
-			double actual = testSetUser.getBalance();
+			double expected = 100.0;
+			testUser.setBalance(expected);
+			double actual = testUser.getBalance();
 			assertEquals(expected, actual);
 		}
 		
@@ -69,97 +97,70 @@ class UserTest {
 		void testSetLog() {
 			ArrayList<String> expected = new ArrayList<String>();
 			expected.add("This is a test log.");
-			testSetUser.setLog(expected);
-			ArrayList<String> actual = testSetUser.getLog();
+			testUser.setLog(expected);
+			ArrayList<String> actual = testUser.getLog();
 			assertEquals(expected, actual);
 		}
 	}
 	
+	@DisplayName("Getter Methods")
 	@Nested
-	@DisplayName("Getters")
 	class getters {
 		
 		@DisplayName("Get Id")
 		@Test
 		void testGetId() {
 			Long expected = 2L;
-			Long actual = testGetUser.getId();
+			Long actual = testUser.getId();
 			assertEquals(expected, actual);
 		}
 		
 		@DisplayName("Get User Id")
 		@Test
-		void testGetUserId() {
-			String expected = "Foobar";
-			String actual = testGetUser.getUserId();
+		void testUserId() {
+			String expected = "N/A";
+			String actual = testUser.getUserId();
 			assertEquals(expected, actual);
 		}
 		
 		@DisplayName("Get Password")
 		@Test
 		void testGetPassword() {
-			String expected = "password";
-			String actual = testGetUser.getPassword();
+			String expected = "";
+			String actual = testUser.getPassword();
 			assertEquals(expected, actual);
 		}
 		
 		@DisplayName("Get Name")
 		@Test
 		void testGetName() {
-			String expected = "John Doe";
-			String actual = testGetUser.getName();
+			String expected = "N/A";
+			String actual = testUser.getName();
 			assertEquals(expected, actual);
 		}
 		
 		@DisplayName("Get Contact Number")
 		@Test
 		void testGetContactNumber() {
-			String expected = "8885558888";
-			String actual = testGetUser.getContactNumber();
+			String expected = "N/A";
+			String actual = testUser.getContactNumber();
 			assertEquals(expected, actual);
 		}
 		
 		@DisplayName("Get Balance")
 		@Test
 		void testGetBalance() {
-			double expected = 100.00;
-			double actual = testGetUser.getBalance();
+			double expected = 0.0;
+			double actual = testUser.getBalance();
 			assertEquals(expected, actual);
 		}
 		
 		@DisplayName("Get Log")
 		@Test
 		void testGetLog() {
-			ArrayList<String> expected = new ArrayList<String>();
-			ArrayList<String> actual = testGetUser.getLog();
+			ArrayList<String> expected = null;
+			ArrayList<String> actual = testUser.getLog();
 			assertEquals(expected, actual);
-		}
-	}
-	
-	@Nested
-	@DisplayName("Constructors")
-	class constructors {
-
-		@DisplayName("Default Values Test")
-		@Test
-		void testDefaultConstructor() {
-			Boolean[] expected = {true, true, true, true, true, true, true};
-			Boolean[] actual = {
-					(testDefaultUser.getId() == 4), 
-					(testDefaultUser.getUserId() == "N/A"), 
-					(testDefaultUser.getPassword() == ""), 
-					(testDefaultUser.getName() == "N/A"), 
-					(testDefaultUser.getContactNumber() == "N/A"), 
-					(testDefaultUser.getBalance() == 0.0), 
-					(testDefaultUser.getLog() == null)
-			};
-			assertArrayEquals(expected, actual);
-		}
-
-		@DisplayName("Auto Incrementing ID Test")
-		@Test
-		void testCurrentIdIncrement() {
-			assertEquals(5L, User.getCurrentID());
 		}
 	}
 	
