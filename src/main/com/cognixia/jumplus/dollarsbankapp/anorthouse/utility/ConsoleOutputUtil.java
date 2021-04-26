f58aec64a39a18666d49d2aea8230c9f7c1d889d
Alexandre Northouse <Alex.J.Northouse@gmail.com>
2021-04-26 13:52:24 -0400

    Move console printing functionality to ConsoleOutputUtil

--- src/main/com/cognixia/jumplus/dollarsbankapp/anorthouse/utility/ConsoleOutputUtil.java
+++ src/main/com/cognixia/jumplus/dollarsbankapp/anorthouse/utility/ConsoleOutputUtil.java
@@ -1,5 +1,9 @@
 package com.cognixia.jumplus.dollarsbankapp.anorthouse.utility;
 
 public class ConsoleOutputUtil {
+	
+	public static void printStatement(String message) {
+		System.out.println(message);
+	}
 
 }
